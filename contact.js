32549a676b508421c33279d75142eba6ec785a75
Shreyas Lad <shadowtemplates@gmail.com>
2018-02-02 11:33:14 -0800

    Added -r contact search feature

--- contact.js
+++ contact.js
@@ -1,10 +1,12 @@
 const program = require('commander');
 const { addContact, getContact } = require('./logic');
 
+// basic program info
 program
   .version('0.0.1')
   .description('Contact management system')
 
+// first function that adds contacts
 program
   .command('addContact <firstname> <lastname> <phone> <email>')
   .alias('-a')
@@ -12,3 +14,11 @@ program
   .action((firstname, lastname, phone, email) => {
     addContact({firstname, lastname, phone, email});
 });
+
+program
+  .command('getContact <name>')
+  .alias('-r')
+  .description('Get a contact that has already been added')
+  .action(name => getContact(name));
+
+program.parse(process.argv);
