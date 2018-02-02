e4369f85e187ed4409817b78b19e8e095acc1b9c
Shreyas Lad <shadowtemplates@gmail.com>
2018-02-02 11:21:12 -0800

    added sum stufz tu contact.js

--- contact.js
+++ contact.js
@@ -1,1 +1,14 @@
 const program = require('commander');
+const { addContact, getContact } = require('./logic');
+
+program
+  .version('0.0.1')
+  .description('Contact management system')
+
+program
+  .command('addContact <firstname> <lastname> <phone> <email>')
+  .alias('-a')
+  .description('Add a contact')
+  .action((firstname, lastname, phone, email) => {
+    addContact({firstname, lastname, phone, email});
+});
